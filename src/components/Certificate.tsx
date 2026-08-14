80c65c03607fdb9605eec3b9a22518eb48e48843
Timothy Tokmang Wang <tokmangwang@gmail.com>
2026-08-14 03:59:08 +0100

    Update Certificte url 2

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -43,7 +43,7 @@ const Certificate: React.FC<CertificateProps> = ({
     }
   };
 
-  const certificateImage = "https://ohpkfpvyiz0fif6h.public.blob.vercel-storage.com/cybersecurity_certificate.png";
+  const certificateImage = "https://ohpkfpvyiz0fif6h.public.blob.vercel-storage.com/img_internal_audit_certificate.png";
 
   return (
     <div className={variant === 'full' ? "flex flex-col items-center" : "inline-block"}>
